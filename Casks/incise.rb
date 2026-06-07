effd9a44e8234ffc199f67eef802dfa75f20afb8
Ivan Histand <ivan@badgeretl.com>
2026-06-07 14:33:03 -0500

    chore(cask): point download url to GitHub release asset

--- Casks/incise.rb
+++ Casks/incise.rb
@@ -2,7 +2,7 @@ cask "incise" do
   version "1.0.0"
   sha256 "9a7ace28b9c0b105e61fd1038b636b933368f6c460a22eb92230fa641cc53612"
 
-  url "https://incise.dev/Incise.dmg"
+  url "https://github.com/ihistand/homebrew-tap/releases/download/v#{version}/Incise.dmg"
   name "Incise"
   desc "Precise native macOS text editor"
   homepage "https://incise.dev/"
