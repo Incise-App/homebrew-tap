5e90dad1e672516707f0f05a5f8a5b79ff9e24fb
Ivan Histand <ivan@badgeretl.com>
2026-06-24 16:00:26 -0500

    incise 1.2.0: correct DMG sha256 (rebuilt with terminal)

--- Casks/incise.rb
+++ Casks/incise.rb
@@ -1,6 +1,6 @@
 cask "incise" do
   version "1.2.0"
-  sha256 "68ccaaa1d3ccafe43310b786373f5f944aaf252aefc966096eb730f4aaf45909"
+  sha256 "86d255a7d53d5204b12ab13ed572bc9d47e09d1b1a96be39977cfa9efa7062c3"
 
   url "https://github.com/ihistand/homebrew-tap/releases/download/v#{version}/Incise.dmg"
   name "Incise"
