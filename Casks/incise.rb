7af96fc9973aa9900e8889158bc7a7bfc117fd42
Ivan Histand <ivan@badgeretl.com>
2026-07-15 22:06:45 -0500

    incise 1.10.0

--- Casks/incise.rb
+++ Casks/incise.rb
@@ -1,6 +1,6 @@
 cask "incise" do
-  version "1.9.5"
-  sha256 "ac035e0154d2708f44dfe28153d58a34218d1613e6c3d8fb4b4eb896e6b7a889"
+  version "1.10.0"
+  sha256 "a115684fb08de5db63469ca7ae427c1344a7e0e17c75129aa747aaf3bdfd9a06"
 
   url "https://github.com/Incise-App/homebrew-tap/releases/download/v#{version}/Incise.dmg"
   name "Incise"
